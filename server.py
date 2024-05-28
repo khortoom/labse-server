cc86335d428cd59b6bb29a011321abc89f969e48
Benyamin.Beyzaie <benyamin137928@gmail.com>
2024-05-28 19:08:30 +0330

    init

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from sentence_transformers import SentenceTransformer
+
+# Load the model
+laBSE_model = SentenceTransformer("setu4993/LaBSE")
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+
+@app.route("/encode", methods=["POST"])
+def encode_query():
+    data = request.json
+
+    query = data.get("query")
+
+    if not query:
+        return jsonify({"error": "No query provided"}), 400
+
+    encoded_query = laBSE_model.encode(query).tolist()
+
+    return jsonify({"vector": encoded_query})
+
+
+if __name__ == "__main__":
+    # Run the Flask app
+    app.run(host="0.0.0.0", port=5000)
